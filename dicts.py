dd61844b89a57631023c2f6c7e6c11735b6be433
Loyuki <shislis@shislis.me>
2013-07-06 22:51:07 +0800

    use iciba instead

--- dicts.py
+++ dicts.py
@@ -6,7 +6,7 @@ class BaseDict:
 		pass
 	def getDef(self, word):
 		raise NotImplementedError()
-class YouDaoDict:
+class _YouDaoDict:
 	def __init__(BaseDict):
 		pass
 	def getDef(self, word):
@@ -16,3 +16,16 @@ class YouDaoDict:
 			defElements = soup.select('#phrsListTab > .trans-container > ul > li')
 			defs = [str(defElement.string) for defElement in defElements]
 			return defs
+class YouDaoDict:
+	def __init__(BaseDict):
+		pass
+	def getDef(self, word):
+		query = urllib.parse.quote_plus(word)
+		with urllib.request.urlopen('http://www.iciba.com/{query}'.format(query=query)) as f:
+			soup = bs4.BeautifulSoup(f, "lxml")
+			defPropEs = soup.select('#dict_main .group_pos > p > .fl')
+			defEs = soup.select('#dict_main .group_pos > p > .label_list > label')
+			defProps = [str(defPropE.string) for defPropE in defPropEs]
+			defs = [str(defE.string) for defE in defEs]
+			defFull = [defProp + ' ' + definition for defProp, definition in zip(defProps, defs)]
+			return defFull
