212be275a4d38e517232d1d37dc46aad1cf85835
Loyuki <shislis@shislis.me>
2013-08-06 20:06:49 +0800

    bug fix

--- handlers.py
+++ handlers.py
@@ -251,7 +251,7 @@ class DefHandler(BaseHandler):
 		d = dicts.CachedYouDaoDict(word, db)
 		defs = " ".join(d.getDef())
 		sens = d.getEgSentence()
-		sen = ""
+		sen = ("", "")
 		if sens != []:
 			sen = random.choice(sens)
 		engSen = sen[0]
